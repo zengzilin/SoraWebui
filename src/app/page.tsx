eedd93ac0a56b1a435e3bcaec33df16add6f2a7c
zengzilin <zengzilin@users.noreply.github.com>
2024-10-10 11:43:39 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import {redirect} from 'next/navigation';
-
+import Link from 'next/link'
 // This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {
   redirect('/en');
+   <Link href="https://aisphereguide.com">
+    <a target="_blank" title="Tap4 AI Tools Directory">
+      AI Sphere Guide
+    </a>
+  </Link>
 }
